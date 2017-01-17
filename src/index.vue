e9bbefaddf2521639a3b831a64c42880aa4a7866
Peng Zhong <peng@nylira.com>
2017-01-17 13:08:56 +0900

    init

---
+++ src/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <header :class="headerClass">
+    <div class="pz-page-header-container">
+      <h1 class="title">{{ title }}</h1>
+      <p class="subtitle" v-if="subtitle" v-html="subtitle"></p>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'pz-page-header',
+  computed: {
+    headerClass () {
+      let value = 'pz-page-header'
+      if (this.type === 'center') {
+        value += ' pz-page-header-center'
+      } else if (this.type === 'wide') {
+        value += ' pz-page-header-wide'
+      } else {
+        value += ' pz-page-header-default '
+      }
+      return value
+    }
+  },
+  props: ['title', 'subtitle', 'type']
+}
+</script>
+
+<style src="./style.css"></style>
