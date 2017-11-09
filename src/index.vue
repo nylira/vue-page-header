0dc9a3fc19f77a78e8882f89e48a6641add0f888
Peng Zhong <peng@nylira.com>
2017-11-09 10:36:13 -0500

    update page header

--- src/index.vue
+++ src/index.vue
@@ -34,4 +34,77 @@ export default {
 }
 </script>
 
-<style src="./style.css"></style>
+<style lang="stylus">
+@require '~@/styles/variables.styl'
+
+.ni-page-header
+  position relative
+  border-bottom 1px dotted bc
+  .ni-page-header-container
+    padding 1.25rem
+    max-width 48rem
+    display flex
+    flex-flow column nowrap
+  .title
+    font-size 2em
+    line-height 1
+    color bright
+    font-weight 600
+  .subtitle
+    color dim
+    margin-top 0.75em
+    font-size 1em
+    font-weight 400
+    line-height 1.25
+
+    strong
+      font-weight 500
+
+  &.ni-page-header-default
+    .ni-page-header-container
+      align-items flex-start
+
+  &.ni-page-header-center
+    .ni-page-header-container
+      align-items center
+      justify-content center
+      align-content center
+      text-align center
+
+  &.ni-page-header-theme-tendermint
+    background hsl(210, 70%, 13%)
+    .title
+      color hsl(210, 70%, 90%)
+    .subtitle
+      color hsl(210, 70%, 50%)
+      strong
+        font-weight 500
+        color hsl(210, 70%, 60%)
+      a
+        color hsl(210, 70%, 70%)
+        border-bottom-color hsl(210, 70%, 50%)
+        &:hover
+          color #fff
+
+@media screen and (min-width: 360px)
+  .ni-page-header
+    .ni-page-header-container
+      padding 1.5rem
+
+@media screen and (min-width: 768px)
+  .ni-page-header
+    .ni-page-header-container
+      padding 3rem
+
+@media screen and (min-width: 1024px)
+  .ni-page-header
+    &.ni-page-header-split
+      border-bottom none
+      .ni-page-header-container
+        padding 3em
+        text-align left
+      .title
+        font-size 2rem
+      .subtitle
+        font-size 1rem
+        letter-spacing 0.02em
